7acf0004c4ca14f4c0797889c4ccea2bc7e0d3a9
Marcin Kisielewski <mkisielewski25@gmail.com>
2020-06-25 12:33:04 +0200

    rebuild js code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
-let currencyValue = document.querySelector(".js-eurValue");
-let currencyExchangeRate = document.querySelector(".js-exchangeRate");
-let formElement = document.querySelector(".js-form")
-let exchangeValue = document.querySelector(".js-exchangeValue")
+let amountElement = document.querySelector(".js-amount");
+let formElement = document.querySelector(".js-form");
+let currencyElement = document.querySelector(".js-currency");
+let exchangeElement = document.querySelector(".js-exchangeValue")
+
+
+let rateEUR = 4.45;
+let rateUSD = 3.95;
+let rateGBP = 4.94;
+let ratePLN = 1;
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let currency = currencyValue.value;
-    let exchange = currencyExchangeRate.value;
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
 
-    let exchangeResult = (currency * exchange)
+    let result;
 
-    exchangeValue.innerText = exchangeResult.toFixed(2);
+    switch (currency) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
+
+        case "USD":
+            result = amount / rateUSD;
+            break;
+
+        case "GBP":
+            result = amount / rateGBP;
+            break;
+
+        case "PLN":
+            result = amount / ratePLN;
+            break;
+    }
+    exchangeElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency} </strong>`;
+
 });
